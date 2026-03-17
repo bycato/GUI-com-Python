901520c31ffaeeedf97a4acce4f9388fe26618dc
cato <antoniocarlosfeliciano6@gmail.com>
2026-03-16 21:32:38 -0400

    Banco de dados

---
+++ sql/db_usuario.sql
@@ -0,0 +1,10 @@
+create table usuario(
+id int not null auto_increment primary key,
+nome varchar(255),
+email varchar(255),
+endereco varchar(255)
+);
+
+select * from usuario;
+
+select * from usuario;
